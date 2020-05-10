56b75ca36a54d365131a96ff710f0f90a413c7c9
Giorgi Guliashvili <guliashvili@users.noreply.github.com>
2020-05-11 00:19:50 +0100

    Fix Strict Null Checking TS issue
    TS does not accept type widening in the strict null checking mode. Setting wide enough type to previews in advance.

--- src/Dropzone.tsx
+++ src/Dropzone.tsx
@@ -630,7 +630,7 @@ class Dropzone extends React.Component<IDropzoneProps, { active: boolean; dragge
     const SubmitButton = SubmitButtonComponent || SubmitButtonDefault
     const Layout = LayoutComponent || LayoutDefault
 
-    let previews = null
+    let previews: JSX.Element[] | null = null
     if (PreviewComponent !== null) {
       previews = files.map(f => {
         return (
