a41fe0eec7e3754b2073dd0859235bdf64e25490
Will Weaver <wweaver@cruisecritic.com>
2022-06-10 17:00:50 -0400

    Attempt to fix

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,6 +1,7 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin");
 const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
 
 if (process.env.NODE_ENV !== 'production') {
   throw new Error('Production builds must have NODE_ENV=production.')
@@ -12,36 +13,51 @@ function createConfig(entry, output) {
     entry,
     output,
     optimization: {
-      minimizer: [new UglifyJSPlugin()],
+      minimizer: [new TerserPlugin()],
     },
     module: {
       rules: [
         {
-          test: /\.js?$/,
-          exclude: /node_modules/,
-          use: 'babel-loader',
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
         },
         {
           test: /\.css$/,
-          loaders: ['style-loader', 'css-loader'],
+          loader: 'style-loader',
         },
         {
           test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
           exclude: /node_modules/,
-          loader: 'url-loader?limit=10000',
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
         },
+
       ],
     },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.js']
+    },
+    plugins: [
+      new CopyPlugin([
+        "src/styles.css",
+        {from: "src/assets/cancel.svg", to: "assets"},
+        {from: "src/assets/remove.svg", to: "assets"},
+        {from: "src/assets/restart.svg", to: "assets"},
+      ]),
+    ]
   }
 }
 
 module.exports = [
-  createConfig('./src/Dropzone.js', {
+  createConfig('./src/Dropzone.tsx', {
     path: path.resolve('dist'),
     libraryTarget: 'commonjs2',
     filename: 'react-dropzone-uploader.js',
   }),
-  createConfig('./src/Dropzone.js', {
+  createConfig('./src/Dropzone.tsx', {
     path: path.resolve('dist'),
     libraryTarget: 'umd',
     filename: 'react-dropzone-uploader.umd.js',
